52831daf6d0994c35760a81cfdd195e4f2599ad3
alexandercurtis <alex.curtis14@gmail.com>
2024-05-09 07:21:31 -0400

    Started on GameResource.  Starting on Client in separate repo to determine which methods will be needed here.

--- src/main/java/com/alexc/gamedataservice/resources/GameResource.java
+++ src/main/java/com/alexc/gamedataservice/resources/GameResource.java
@@ -1,12 +1,35 @@
 package com.alexc.gamedataservice.resources;
 
+import com.alexc.gamedataservice.models.Game;
 import com.alexc.gamedataservice.services.GameService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
+@RequestMapping("/games")
 public class GameResource {
     @Autowired
     GameService gameService;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Game> getGame(@PathVariable("id") Long id) {
+        ResponseEntity<Game> returnedGame = restTemplate.getForEntity("http://localhost:8082/games/" + id, Game.class);
+        if(returnedGame.getStatusCode() == HttpStatus.OK) {
+            return returnedGame;
+        }
+        return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
+
+
 
 }
